5168945438cb7b91599653c545d365df1d25e6b3
구본민 <gbm@gfs.co.kr>
2024-08-20 13:54:55 +0900

    * SysLinker
    - 프로젝트 설정
    
    1. Debug_SLP4 / Release_SLP4 빌드 버전 추가
    
    - Version
    
    1. SLP4용 버전 정보 추가

--- Version/Version.h
+++ Version/Version.h
@@ -1,7 +1,11 @@
 #define	GFSP_SW_VERSION			1
 #define GFSP_PRODUCT_VERSION		"1.13"
 #define GFSP_PRODUCT_VERSION_NUM	1, 13, 0, 8
+#ifndef SLP4_MODE
 #define GFSP_PRODUCT_UPDATE_DATE	"GFSP v1.13 , Last Updated On 13 September 2023"
+#else
+#define GFSP_PRODUCT_UPDATE_DATE	"SLP4 v1.00 , Last Updated On 1 October 2024"
+#endif
 #define GFSP_PRODUCT_NAME			"GFSP"
 #define GFSP_PROGRAM_NAME			"GFSP.EXE"
 
